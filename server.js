492fa149eee203499611bf96ce6b3103201868f4
Adrien Vidal <adriensg.vidal@gmail.com>
2020-11-08 14:05:50 +0100

    add server routes

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.get('/', (req, res) => res.json({msg: 'Welcome !'}));
 
+
+// Define routes
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/contacts', require('./routes/contacts'))
+
+
+// start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
