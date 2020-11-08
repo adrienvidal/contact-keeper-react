136f04b44fc057fbc5741c17e2c8cd7e06a2c7dd
Adrien Vidal <adriensg.vidal@gmail.com>
2020-11-08 16:31:35 +0100

    add jsonwebtoken

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { body, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -48,13 +50,31 @@ router.post(
 
       // save to database
       await user.save();
-      res.send('User saved');
+
+      // send json web token for authentification in client
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 36000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+      // res.send('User saved');
     } catch (error) {
       console.error(err.message);
       res.status(500).send('Server error');
     }
-
-    res.send('passed');
   }
 );
 
