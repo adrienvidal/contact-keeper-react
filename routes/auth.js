75b82b01ef819c8150cb1deea528b6d6f85da7b9
Adrien Vidal <adriensg.vidal@gmail.com>
2020-11-09 14:50:50 +0100

    add login DB

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { body, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route   GET api/auth
 // @desc    get logged in user
@@ -11,8 +17,59 @@ router.get('/', (req, res) => {
 // @route   POST api/auth
 // @desc    Auth user & get token
 // @access  Public
-router.post('/', (req, res) => {
-    res.send('Log in user');
-  });
+router.post(
+  '/',
+  [
+    body('email', 'Please include a valide email').isEmail(),
+    body('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    // if errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // if no errors
+    const { email, password } = req.body;
+
+    try {
+      // check is user exist
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid credentials' });
+      }
+
+      // check if password match the one in DB
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+      }
+
+      // send json web token for authentification in client
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 36000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -21,8 +21,8 @@ router.post(
     ).isLength({ min: 6 }),
   ],
   async (req, res) => {
-    const errors = validationResult(req);
     // if errors
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
@@ -71,7 +71,7 @@ router.post(
       );
 
       // res.send('User saved');
-    } catch (error) {
+    } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
     }
