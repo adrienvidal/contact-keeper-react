754e8fe66c30caefd9459b9a4f3a27a70b750c88
Adrien Vidal <adriensg.vidal@gmail.com>
2020-11-17 15:48:30 +0100

    add privateRoutes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alerts from './components/layout/Alerts';
+import PrivateRoutes from './components/routing/PrivateRoutes';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
-import setAuthToken from './utils/setAuthToken'
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
 
 // set auth tokin in axios header
@@ -29,7 +30,7 @@ const App = () => {
               <div className='container'>
                 <Alerts />
                 <Switch>
-                  <Route exact path='/' component={Home} />
+                  <PrivateRoutes exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} />
